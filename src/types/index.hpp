3e3d31a5aced21894ea6f6d69ad656cef606b0fc
Andrew Stelter <andrew.d.stelter@gmail.com>
2024-01-13 10:17:06 -0600

    Make board::Index constexpr
    Now we can generate sets of indices to iterate over at compile time. for funsies!

--- src/types/index.hpp
+++ src/types/index.hpp
@@ -9,19 +9,21 @@ namespace sudoku::types::board {
 class Index
 {
   public:
-    Index() : Index(0, 0){};
+    constexpr Index() : Index(0, 0){};
 
     template<std::integral T, std::integral U>
-    Index(T row, U col) : _row(static_cast<std::uint8_t>(row)), _col(static_cast<std::uint8_t>(col))
+    constexpr Index(T row, U col) :
+        _row(static_cast<std::uint8_t>(row)), _col(static_cast<std::uint8_t>(col))
     {
         SUDOKU_ASSERT(row >= 0 && row < 9);
         SUDOKU_ASSERT(col >= 0 && col < 9);
     }
 
-    std::uint8_t row() const { return _row; }
-    std::uint8_t col() const { return _col; }
+    constexpr std::uint8_t row() const { return _row; }
+    constexpr std::uint8_t col() const { return _col; }
 
-    bool operator!=(const Index& other) const = default;
+    constexpr bool operator==(const Index& other) const = default;
+    constexpr bool operator!=(const Index& other) const = default;
 
   private:
     std::uint8_t _row;
