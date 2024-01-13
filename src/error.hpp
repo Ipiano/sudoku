4d06a190c21f25513607af4b2b05c2ca264759b9
Andrew Stelter <andrew.d.stelter@gmail.com>
2024-01-13 13:04:39 -0600

    Fix access scopes to error exceptions

--- src/error.hpp
+++ src/error.hpp
@@ -3,17 +3,17 @@
 #include <stdexcept>
 
 namespace sudoku {
-class Error : std::runtime_error
+class Error : public std::runtime_error
 {
     using std::runtime_error::runtime_error;
 };
 
-class AssertionFailure : Error
+class AssertionFailure : public Error
 {
   public:
     AssertionFailure(const char* file, int line, const char* assertion) :
         Error("Assertion failed '" + std::string(assertion) + "' @ " + std::string(file) + ":" +
-              std::to_string(line))
+              std::string(assertion))
     {
     }
 };
